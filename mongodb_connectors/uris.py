4407f98d1c41743cdfc484441db505e1477f4fbf
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2025-05-17 23:26:41 +0530

    Create uris.py

---
+++ mongodb_connectors/uris.py
@@ -0,0 +1,44 @@
+# Pre-configured URIs (consider using environment variables for production)
+URI_DATABASE = {
+    'bikash_primary': 'mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority',
+  
+    'bikash_secondary': 'mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority',
+    
+    'zomato_master': 'mongodb+srv://AdityaGusain:AdityaGusain@zomato-master.3flxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+    'badhacker': 'mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/',
+ 
+    'ajibade_cluster': 'mongodb+srv://ajibadee:beowulfa@cluster0.oq1hdin.mongodb.net/?retryWrites=true&w=majority',
+
+    'sana_cluster': 'mongodb+srv://sanasomani786:TJgADfpkI1XVUkKt@cluster0.ruhyad9.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+ 
+    'bsdk_cluster': 'mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority',
+
+    'anonx_music': 'mongodb+srv://anonxmusic:anonxmusic@anonx.9v4i8.mongodb.net/?retryWrites=true&w=majority',
+ 
+    'asad_ali': 'mongodb+srv://AsadAli:AsadAli@cluster0.3ejv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+    'rr_cluster': 'mongodb+srv://rr:rr@cluster0.5pjchfv.mongodb.net/?retryWrites=true&w=majority',
+  
+    'branded_world': 'mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority',
+  
+    'hnyx_cluster': 'mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority',
+   
+    'ravi_cluster': 'mongodb+srv://ravi:ravi12345@cluster0.hndinhj.mongodb.net/?retryWrites=true&w=majority',
+   
+    'userbot': 'mongodb+srv://userbot:userbot@cluster0.iweqz.mongodb.net/test?retryWrites=true&w=majority',
+    
+    'alisha_cluster': 'mongodb+srv://Alisha:Alisha123@cluster0.yqcpftw.mongodb.net/?retryWrites=true&w=majority',
+
+    'vikash_cluster': 'mongodb+srv://vikashgup87:EDRIe3bdEq85Pdpl@cluster0.pvoygcu.mongodb.net/?retryWrites=true&w=majority',
+
+    'sarkar': 'mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority',
+    
+    'kuldip_cluster': 'mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority',
+    
+    'krishna_cluster': 'mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority',
+    
+    'rahul_cluster': 'mongodb+srv://rahul:rahulkr@cluster0.szdpcp6.mongodb.net/?retryWrites=true&w=majority',
+    
+    'knight_rider': 'mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority'
+}
